7eccc5f8f9bbc05144135e40b6f5bab3a1a0f57e
feelyd-reference <feelyd@u34e6d712510557419e61.ant.amazon.com>
2016-10-01 14:28:03 +0100

    Upgrade npm

--- npm/recipes/setup.rb
+++ npm/recipes/setup.rb
@@ -1,4 +1,10 @@
 execute 'Execute my script' do 
   user    'root'
+  command 'npm -g install npm'
+end
+
+execute 'Execute my script' do 
+  user    'root'
   command 'sudo npm config set registry http://registry.npmjs.org/'
 end
+
